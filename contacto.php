a77af7fc46403bf263a3368af0ce99887af260b5
celinatarn <celina_tarn@hotmail.com>
2017-09-27 14:24:58 -0300

    Auto stash before merge of "master" and "origin/master"

--- contacto.php
+++ contacto.php
@@ -1,3 +1,39 @@
+<?php
+$enviar_a = "misfinanzasDH@gmail.com";
+$asunto = "Consulta - Mis Finanzas";
+$errores=[];
+//
+if($_POST) {
+  if (empty($_POST['name']) || preg_match('/[^A-Za-z0-9]/', $_POST['name'])) {
+    $errores['name'] = 'Debe ingresar un nombre válido';
+  }
+  if(empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $errores['email'] = 'Debe ingresar un e-mail válido';
+  }
+  if (empty($_POST['comment'])) {
+    $errores['comment'] = 'Por favor ingrese una consulta';
+  } else {
+    $mensaje = htmlspecialchars($_POST['comment']);
+    $mensaje = trim($mensaje);
+    $mensaje = stripslashes($mensaje);
+  }
+  $name = $_POST['name'];
+  $from = $_POST['email'];
+}
+
+if (empty($errores)) {
+
+  $mensaje_enviar = "De $name \n";
+  $mensaje_enviar = "Correo: $from \n";
+  $mensaje_enviar = 'Mensaje: '.$mensaje;
+
+  mail($enviar_a, $asunto, $mensaje_enviar);
+}
+
+
+
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,31 +47,41 @@
   </head>
   <body>
     <div class="container">
-      <div class="header">
         <?php include('header/header.html') ?>
       <div class="contacto">
         <div class="form">
           <div class="backgroundColor">
             <h2>Envianos tus inquietudes</h2>
-            <form class="contacto" action="index.html" method="post">
+            <form class="contacto" action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
               <label for="name">Tu Nombre</label>
-              <input id="name" type="text" name="name" value="">
+              <input id="name" type="text" name="name" value="<?php if (empty($errores['name'])) {
+                echo $name;
+              } ?>">
               <br>
               <label for="email">Tu E-mail</label>
-              <input id="email" type="text" name="email" value="">
+              <input id="email" type="text" name="email" value="<?php if (empty($errores['email'])) {
+                echo $from;
+              } ?>">
               <br>
-            </form>
             <label class="comment" for="comment">Comentario</label>
-            <textarea name="comment" rows="8" cols="80"></textarea>
+            <textarea name="comment" rows="8" cols="80"><?php if (empty($errores['comment'])) {
+              echo $mensaje;
+            } ?></textarea>
             <button type="submit" name="button">Enviar</button>
+          </form>
+          <?php
+            if (!empty($errores)) {
+              foreach ($errores as $key => $value) {
+                echo $value.'<br>';
+              }
+            }
+           ?>
           </div> <!-- .backgroundColor -->
-        </div>
-      </div>
-      <footer class="footerIndex">
-        <img class="margin-bottom" src="images/logo200px.png" alt="" width="200px">
-        <p class="margin-bottom"><a href="#">Terminos & Condiciones</a> <a href="#">Póliza de Privacidad</a><a href="#">Copyrights</a></p>
-        <p class="copyright">Copyright (c) 2017 Copyright Holder All Rights Reserved.</p>
-      </footer>
+
+        </div> <!-- .form -->
+    </div> <!-- .container -->
+    <div class="footer">
+      <?php include('footer/footer.html') ?>
     </div>
   </body>
 </html>
--- preferencias.php
+++ preferencias.php
@@ -17,13 +17,29 @@
         <?php include('./profile_menu/profile_menu.php') ?>
       <h1>PREFERENCIAS</h1>
         <form class="preferencias" action="preferencias.php" method="post">
+          <h2>Cambiar datos personales</h2>
           <label for="username">Nombre de Usuario</label>
           <input id="username" type="text" name="username" value="'USER_NAME'">
           <br>
           <label for="nombre">Nombre</label>
           <input id="nombre" type="text" name="" value="">
+          <br>
+          <label for="apellido">Apellido</label>
+          <input id="apellido" type="text" name="" value="">
+          <br>
+          <label for="oldpassword">Contraseña Actual</label>
+          <input id="oldpassword" type="password" name="oldpassword" value="">
+          <br>
+          <label for="password">Nueva Contraseña</label>
+          <input id="password" type="password" name="password" value="">
+          <br>
+          <label for="confirmpassword">Confirmar Contraseña</label>
+          <input id="confirmpassword" type="password" name="" value="">
+          <br>
+          <button type="submit" name="button">Editar datos</button>
         </form>
       </div> <!-- .contenido -->
     </div> <!-- .container -->
+    <?php include('footer/footer.html') ?>
   </body>
 </html>
